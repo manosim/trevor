0d52f5cac1de278b1e2d991eedf9ae84e281a963
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-01-20 23:12:03 +0000

    Remove "travispro" on logout
    **FIXED**

--- www/js/controllers/accounts.js
+++ www/js/controllers/accounts.js
@@ -26,6 +26,7 @@ app.controller('AccountsCtrl', function($scope, $state, $window, RequestService,
     $scope.logOut = function() {
         delete $window.localStorage.githubtoken;
         delete $window.localStorage.travistoken;
+        delete $window.localStorage.travispro;
         $state.go('welcome');
     };
 
