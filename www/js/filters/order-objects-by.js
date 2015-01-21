de352f4d3f9218b35534e71ad6f94005900596f9
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-01-21 20:10:12 +0000

    Active Repos - Toggle

--- www/js/filters/order-objects-by.js
+++ www/js/filters/order-objects-by.js
@@ -1,15 +1,36 @@
 angular.module('filter.orderObjectBy', [])
 
 .filter('orderObjectBy', function() {
-  return function(items, field, reverse) {
-    var filtered = [];
-    angular.forEach(items, function(item) {
-      filtered.push(item);
-    });
-    filtered.sort(function (a, b) {
-      return (a[field] > b[field] ? 1 : -1);
-    });
-    if(reverse) filtered.reverse();
-    return filtered;
-  };
+    return function(items, field, reverse) {
+        var filtered = [];
+
+        angular.forEach(items, function(item) {
+            filtered.push(item);
+        });
+
+        filtered.sort(function (a, b) {
+            return (a[field] > b[field] ? 1 : -1);
+        });
+
+        if(reverse) filtered.reverse();
+
+        return filtered;
+    };
+})
+
+
+.filter('with', function() {
+    return function(items, field, onlyActive) {
+        var result = {};
+        angular.forEach(items, function(value, key) {
+            if (onlyActive) {
+                if (value[field] === true) {
+                    result[key] = value;
+                }
+            } else {
+                result[key] = value;
+            }
+        });
+        return result;
+    };
 });
