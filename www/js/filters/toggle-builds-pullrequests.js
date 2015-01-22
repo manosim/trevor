55df0d1baccf8ca27980fe10a6ad53801ad6c141
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-01-22 20:06:37 +0000

    Filter by 'pull_request' in array for right order

--- www/js/filters/toggle-builds-pullrequests.js
+++ www/js/filters/toggle-builds-pullrequests.js
@@ -1,19 +1,22 @@
 angular.module('filter.toggleBuildsPullRequests', [])
 
 .filter('with', function() {
+
     return function(items, field, showOnly) {
-        var result = {};
+
+        var result = [];
         angular.forEach(items, function(value, key) {
             if (showOnly) {
                 if (value[field] === true) {
-                    result[key] = value;
+                    result.push(value);
                 }
             } else if (!showOnly) {
                 if (value[field] === false) {
-                    result[key] = value;
+                    result.push(value);
                 }
             }
         });
+
         return result;
     };
 });
