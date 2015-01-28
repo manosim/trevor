e209ad55d4335a28bad15cfea252cba605fb02c5
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-01-28 22:36:41 +0000

    Reorder uninstalling plugins

--- plugins.sh
+++ plugins.sh
@@ -3,10 +3,10 @@ ionic plugin rm org.apache.cordova.console
 ionic plugin rm org.apache.cordova.device
 ionic plugin rm com.ionic.keyboard
 ionic plugin rm org.apache.cordova.statusbar
-ionic plugin rm org.apache.cordova.dialogs
-ionic plugin rm org.apache.cordova.inappbrowser
 ionic plugin rm com.danielcwilson.plugins.googleanalytics
 ionic plugin rm org.pushandplay.cordova.apprate
+ionic plugin rm org.apache.cordova.dialogs
+ionic plugin rm org.apache.cordova.inappbrowser
 
 # Install Plugins
 ionic plugin add org.apache.cordova.console
