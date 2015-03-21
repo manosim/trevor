b49e6eda6b6f74b6387cfce3314b8b79b67ce53f
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-03-21 16:05:09 +0000

    Init Builds / Show all - show builds - show PRs

--- www/js/filters/toggle-builds-pullrequests.js
+++ www/js/filters/toggle-builds-pullrequests.js
@@ -2,18 +2,20 @@ angular.module('filter.toggleBuildsPullRequests', [])
 
 .filter('with', function() {
 
-    return function(items, field, showOnly) {
+    return function(items, showOnly) {
 
         var result = [];
         angular.forEach(items, function(value, key) {
-            if (showOnly) {
-                if (value[field] === true) {
+            if (showOnly == "builds") {
+                if (value['pull_request'] === false) {
                     result.push(value);
                 }
-            } else if (!showOnly) {
-                if (value[field] === false) {
+            } else if (showOnly == "pr") {
+                if (value['pull_request'] === true) {
                     result.push(value);
                 }
+            } else {
+                result.push(value);
             }
         });
 
