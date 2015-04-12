e969c727b93ee1aec6e05a4b161c0965843a8b4d
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-04-12 04:08:54 +0100

    Working "Search" tab

--- www/js/controllers/search.js
+++ www/js/controllers/search.js
@@ -2,9 +2,42 @@ angular.module('controller.search', [])
 
 .controller('SearchCtrl', function($scope, LoadingService, RequestService, AlertService) {
 
-    $scope.doSearch = function() {
+    $scope.doSearch = function(slug, author) {
         LoadingService.show();
-        LoadingService.hide();
+
+        var searchUrl = "/repos?orderBy=name&active=true";
+
+        if (slug) {
+            searchUrl = searchUrl + "&search=" + slug;
+        }
+
+        if (author) {
+            searchUrl = searchUrl + "&owner_name=" + author;
+        }
+
+        RequestService
+            .request("GET", searchUrl, false, false)
+
+            .then(function(data) {
+
+                console.log("Success-Search!");
+                $scope.results = [];
+                angular.forEach(data.repos, function(value, key) {
+                    value.login_id = value.slug.split("/")[0];
+                    value.short_slug = value.slug.split("/")[1];
+                    $scope.results.push(value);
+                });
+
+                LoadingService.hide();
+
+            }, function(data) {
+
+                // Failure
+                AlertService.raiseAlert("Oops! We couldn't get the search results from Travis CI. Please try again.");
+                LoadingService.hide();
+
+            });
+
     };
 
 });
