bb039062c5814a7e06ac875818b53801442862ee
Emmanouil Konstantinidis <manos@dabapps.com>
2015-09-19 21:59:41 +0100

    Rename Project

--- index.ios.js
+++ index.ios.js
@@ -12,7 +12,7 @@ var {
   View,
 } = React;
 
-var trevor = React.createClass({
+var Trevor = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
@@ -50,4 +50,4 @@ var styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('trevor', () => trevor);
+AppRegistry.registerComponent('Trevor', () => Trevor);
