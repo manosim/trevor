e54d3de3729830d3290ba0d709c27c7cef9a8785
Emmanouil Konstantinidis <manos@dabapps.com>
2015-09-19 19:29:58 +0100

    Clean Version One

--- www/js/controllers/about.js
+++
@@ -1,9 +0,0 @@
-angular.module('controller.about', [])
-
-.controller('AboutCtrl', function($scope, $stateParams, $window) {
-
-    $scope.goTo = function (url) {
-        $window.open(url, '_system');
-    } ;
-
-});
