1c2825ab4a1aef93f82929f9a4e44a29ba33a827
Emmanouil Konstantinidis <manos@dabapps.com>
2015-09-20 01:54:47 +0100

    Split BuildsScreen & index.ios

---
+++ BuildsScreen.js
@@ -0,0 +1,98 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var moment = require('moment');
+
+var {
+  StyleSheet,
+  Text,
+  View,
+  ListView
+} = React;
+
+var BuildsScreen = React.createClass({
+  getInitialState: function() {
+    return {
+      loading: false,
+      builds: new ListView.DataSource({
+          rowHasChanged: (row1, row2) => row1 !== row2,
+        }).cloneWithRows([])
+    };
+  },
+
+  componentWillMount: function() {
+    var self = this;
+    fetch('https://api.travis-ci.org/repos/ekonstantinidis/gitify/builds', {
+      headers: {
+        'Accept': 'application/vnd.travis-ci.2+json'
+      }
+    })
+      .then(function(response) {
+        return response.json()
+      }).then(function(json) {
+        self.setState({
+          loading: false,
+          builds: self.state.builds.cloneWithRows(json.builds)
+        });
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  },
+
+  _renderBuildRow: function (rowData: string, sectionID: number, rowID: number) {
+    var finishedDate = moment(rowData.finished_at).format("YYYY-MM-DD")
+
+    return (
+      <View style={styles.buildRow}>
+        <View style={styles.buildStatus}>
+          <Text>✔︎</Text>
+          <Text>{rowData.number}</Text>
+        </View>
+        <View style={styles.buildInfo}>
+          <Text>{rowData.id}</Text>
+          <Text>{finishedDate}</Text>
+          <Text>{rowData.state}</Text>
+        </View>
+      </View>
+    );
+  },
+
+  render: function() {
+    return (
+      <ListView
+        dataSource={this.state.builds}
+        renderRow={this._renderBuildRow}
+        style={styles.container} />
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF'
+  },
+  buildRow: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 0,
+    marginBottom: 1
+  },
+  buildStatus: {
+    flex: 0.1,
+    padding: 10,
+    backgroundColor: 'red',
+    color: 'white'
+  },
+  buildInfo: {
+    flex: 0.9,
+    padding: 10
+  }
+});
+
+module.exports = BuildsScreen;
--- index.ios.js
+++ index.ios.js
@@ -1,74 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
-var moment = require('moment');
-
 var {
   AppRegistry,
+  NavigatorIOS,
   StyleSheet,
-  Text,
-  View,
-  ListView
 } = React;
 
+var BuildsScreen = require('./BuildsScreen');
+
 var Trevor = React.createClass({
-  getInitialState: function() {
-    return {
-      loading: false,
-      builds: new ListView.DataSource({
-          rowHasChanged: (row1, row2) => row1 !== row2,
-        }).cloneWithRows([])
-    };
-  },
-
-  componentWillMount: function() {
-    var self = this;
-    fetch('https://api.travis-ci.org/repos/ekonstantinidis/gitify/builds', {
-      headers: {
-        'Accept': 'application/vnd.travis-ci.2+json'
-      }
-    })
-      .then(function(response) {
-        return response.json()
-      }).then(function(json) {
-        self.setState({
-          loading: false,
-          builds: self.state.builds.cloneWithRows(json.builds)
-        });
-      })
-      .catch((error) => {
-        console.warn(error);
-      });
-  },
-
-  _renderBuildRow: function (rowData: string, sectionID: number, rowID: number) {
-    var finishedDate = moment(rowData.finished_at).format("YYYY-MM-DD")
-
-    return (
-      <View style={styles.buildRow}>
-        <View style={styles.buildStatus}>
-          <Text>✔︎</Text>
-          <Text>{rowData.number}</Text>
-        </View>
-        <View style={styles.buildInfo}>
-          <Text>{rowData.id}</Text>
-          <Text>{finishedDate}</Text>
-          <Text>{rowData.state}</Text>
-        </View>
-      </View>
-    );
-  },
-
   render: function() {
     return (
-      <ListView
-        dataSource={this.state.builds}
-        renderRow={this._renderBuildRow}
-        style={styles.container} />
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Builds',
+          component: BuildsScreen,
+        }}
+      />
     );
   }
 });
@@ -76,23 +26,10 @@ var Trevor = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF'
+    backgroundColor: 'white',
   },
-  buildRow: {
-    flex: 1,
-    flexDirection: 'row',
-    padding: 0,
-  },
-  buildStatus: {
-    flex: 0.2,
-    padding: 10,
-    backgroundColor: 'red',
-    color: 'white'
-  },
-  buildInfo: {
-    flex: 0.8,
-    padding: 10
-  }
 });
 
 AppRegistry.registerComponent('trevor', () => Trevor);
+
+module.exports = Trevor;
