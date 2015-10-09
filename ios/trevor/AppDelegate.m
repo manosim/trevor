7c015982558405d78ae53c7b55af99c641ebc6ef
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-10-09 21:20:31 +0100

    Remove Deep Linking

--- ios/trevor/AppDelegate.m
+++ ios/trevor/AppDelegate.m
@@ -10,7 +10,6 @@
 #import "AppDelegate.h"
 
 #import "RCTRootView.h"
-#import "RCTLinkingManager.h"
 
 @implementation AppDelegate
 
@@ -57,10 +56,6 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
-}
-
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
-  return [RCTLinkingManager application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
 }
 
 @end
