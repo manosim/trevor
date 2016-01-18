b98f89d310d6bd91e45fe44231657e85b42927fd
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-01-18 21:29:33 +0000

    Fix Divider Layout

--- App/Helpers/Divider.js
+++ App/Helpers/Divider.js
@@ -5,23 +5,28 @@ import Constants from '../Utils/Constants';
 var {
   StyleSheet,
   Text,
+  View
 } = React;
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingVertical: 7,
-    paddingHorizontal: 10,
     backgroundColor: Constants.THEME_DARK_BLUE,
+    paddingHorizontal: 10,
+    paddingTop: 5,
+    paddingBottom: 7,
+  },
+  text: {
     color: '#FFF',
-    fontSize: 16
+    fontSize: 16,
   }
 });
 
 export default class Divider extends React.Component {
   render() {
     return (
-      <Text style={styles.container}>{this.props.text}</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>{this.props.text}</Text>
+      </View>
     );
   }
 };
