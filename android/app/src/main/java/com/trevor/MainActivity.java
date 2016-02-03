f54792f51390d820d32a702c77204c60f9452a0e
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-02-03 22:41:56 +0000

    Merge remote-tracking branch 'origin/android-user-agent' into android-user-agent
    # Conflicts:
    #	package.json

--- android/app/src/main/java/com/trevor/MainActivity.java
+++ android/app/src/main/java/com/trevor/MainActivity.java
@@ -6,9 +6,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
 import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.trevor.CustomInterceptor;
-// import com.trevor.CustomHttpProvider;
+import com.facebook.react.modules.network.OkHttpClientProvider;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,20 +14,20 @@ import java.util.List;
 public class MainActivity extends ReactActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        attachInterceptor();
+    }
 
-      OkHttpClient client = new OkHttpClient();
-      client.networkInterceptors().add(new CustomInterceptor());
-      // httpBuilder.addNetworkInterceptor(new CustomInterceptor());
-      // addNetworkInterceptor
-
-    };
+    private void attachInterceptor() {
+        OkHttpClient client = OkHttpClientProvider.getOkHttpClient();
+        client.networkInterceptors().add(new CustomInterceptor());
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
-     */
+    */
     @Override
     protected String getMainComponentName() {
         return "Trevor";
